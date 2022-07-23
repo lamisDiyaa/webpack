9d61e572a8a7e611df816f53cb6b2557221912e6
lamisDiyaa <lamiiis.diyaa@gmail.com>
2022-07-24 01:21:42 +0200

    second

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import hello from './hello world';
+hello()
+
+
+import "./style.css"
+
+
+import header from "./component/header/header"
+
+
+
+
+
+
+
+
+
+
+
+
+import bio from "./component/bio/bio"
+import photo from"./assets/cover.jpg"
+
+
+/* 
+this is a big comment
+ */
